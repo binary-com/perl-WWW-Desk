2df71a491941a2dd2cf6f80e93c6f731ca74f0bc
Rakesh Kumar Shardiwal <rakesh.shardiwal@gmail.com>
2015-05-13 08:27:10 +0530

    Single Access Token work starting

---
+++ lib/WWW/Desk/Auth/oAuth/SingleAccessToken.pm
@@ -0,0 +1,205 @@
+package WWW::Desk::Auth::oAuth::SingleAccessToken;
+
+use 5.006;
+use strict;
+use warnings;
+
+use Moose;
+
+extends 'WWW::Desk::Auth::oAuth';
+
+=head1 NAME
+
+WWW::Desk::Auth::oAuth::SingleAccessToken - Desk.com SingleAccessToken Authentication
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 ATTRIBUTES
+
+=head2 access_token
+
+REQUIRED - desk.com access token
+
+=cut
+
+has 'access_token' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+=head2 access_secret
+
+REQUIRED - desk.com access secret
+
+=cut
+
+has 'access_secret' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'callback_url' => (
+    is         => 'ro',
+    isa        => 'URI',
+    lazy_build => 1
+);
+sub _build_callback_url {
+    my($self) = @_;
+    return Mojo::URL->new;
+}
+
+has 'oauth_token' => (
+    is         => 'ro',
+    isa        => 'Str',
+    lazy_build => 1
+);
+
+sub _build_oauth_token {
+    my($self) = @_;
+    return $self->request_access_token(
+        $self->access_token,
+        $self->access_secret
+    );
+}
+
+=head2 call
+
+call 
+
+=cut
+
+sub call {
+    my ( $self, $url_fragment, $http_method, $params ) = @_;
+
+    if ( not defined $params ) {
+        $params = { 't' => time() };
+    }
+
+    return $self->_prepare_response( "400",
+        "Invalid HTTP method. Only supported GET, POST, PATCH, DELETE" )
+      unless $http_method =~ /^GET$|^POST$|^PATCH$|^DELETE$/i;
+
+    $http_method = lc $http_method;
+
+    my $request_url = $self->build_api_url($url_fragment);
+    my $oauth_token = $self->oauth_token;
+
+    # Use the access token to fetch a protected resource
+    my $response = $oauth_token->get( $request_url );
+
+    if ( not $response->is_success ) {
+        my $error = $response->status_line;
+        return $self->_prepare_response( 404, $error );
+    }
+
+    return $self->_prepare_response( 200, 'OK', $response->decoded_content );
+}
+
+sub _prepare_response {
+    my ( $self, $code, $msg, $data ) = @_;
+    $data = $self->browser_client->json->decode($data) if $data;
+    return {
+        'code'    => $code,
+        'message' => $msg,
+        'data'    => $data
+    };
+}
+
+=head1 AUTHOR
+
+binary.com, C<< <rakesh at binary.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-desk at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Desk>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WWW::Desk
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-Desk>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-Desk>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-Desk>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-Desk/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 binary.com.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+no Moose;
+__PACKAGE__->meta->make_immutable();
+
+1;    # End of WWW::Desk::Auth::oAuth
